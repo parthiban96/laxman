e98d766beb2af7a07b488533968565429906eec8
Parthiban Thangaraj <83872929+parthiban96@users.noreply.github.com>
2022-01-25 22:52:05 +0530

    Add files via upload

---
+++ timer.js
@@ -0,0 +1,42 @@
+setTimeout(() => {
+    document.querySelector("div").innerHTML = 10;
+    setTimeout(() => {
+        document.querySelector("div").innerHTML = 9;
+        setTimeout(() => {
+            document.querySelector("div").innerHTML = 8;
+            setTimeout(() => {
+                document.querySelector("div").innerHTML = 7;
+                setTimeout(() => {
+                    document.querySelector("div").innerHTML = 6;
+                    setTimeout(() => {
+                        document.querySelector("div").innerHTML = 5;
+                        setTimeout(() => {
+                            document.querySelector("div").innerHTML = 4;
+                            setTimeout(() => {
+                                document.querySelector("div").innerHTML = 3;
+                                setTimeout(() => {
+                                    document.querySelector("div").innerHTML = 2;
+                                    setTimeout(() => {
+                                        document.querySelector("div").innerHTML = 1;
+                                        setTimeout(() => {
+                                            document.querySelector("div").innerHTML = ("Happy");
+                                            setTimeout(() => {
+                                                document.querySelector("div").innerHTML = ("Birthday");
+                                                setTimeout(() => {
+                                                    document.querySelector("div").innerHTML = (" Laxman Muthiyah !!!");
+                                                    setTimeout(() => {
+                                                        document.querySelector("div").innerHTML = (" Have a great day...");
+                                                    }, 3000);
+                                                }, 1000);
+                                            }, 1000);  
+                                        }, 1000);
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000);
